6943385893c6e0aa80391eb133203b77ff22d05a
Sumit Gupta <65439917+thesumitg@users.noreply.github.com>
2023-03-29 00:01:02 +0530

    Add files via upload

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const { userRegister, userLogin, logout }=require("../controllers/userSchoolController");
 
+const router = express.Router();
+
+router.route("/register").post(userRegister);
+
+router.route("/login").post(userLogin);
+router.route("/logout").get(logout);
+
+module.exports = router;
