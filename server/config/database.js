33a0027cf0b8130b7e936940b4609756ca30a33e
Sumit Gupta <65439917+thesumitg@users.noreply.github.com>
2023-03-29 12:00:03 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const connectDatabase =()=>{
-    mongoose.connect(process.env.DB_URI,
+
+MONGODB_DATABASE = 'schoolmanagement'
+MONGODB_DATABASE = 'schoolmanagement'
+DB_URI= `mongodb+srv://${process.env.MONGODB_USERNAME}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@cluster0.gzy06ye.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
+    mongoose.connect(DB_URI,
         {
             useNewURlParser:true,
             useUnifiedTopology:true,
