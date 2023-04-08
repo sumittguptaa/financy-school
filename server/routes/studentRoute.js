438f1daf9184e9ed7e8463f2b104a10825ec1ff2
Sumit Gupta <65439917+thesumitg@users.noreply.github.com>
2023-04-08 21:16:36 +0530

    Update studentRoute.js

--- server/routes/studentRoute.js
+++ server/routes/studentRoute.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const { getAllStudents,getAllStudentsAssociatewithSchoolId, createStudent, updateStudent, deleteStudent, getStudentDetails } = require('../controllers/studentController');
-
+const { getAllStudents,getAllStudentsClassWise, createStudent, updateStudent, deleteStudent, getStudentDetails, getAllStudentsAssociatewithSchoolId } = require('../controllers/studentController');
 const router = express.Router();
-
-
 router.route("/students").get(getAllStudents);
 router.route("/students/:schoolId").get(getAllStudentsAssociatewithSchoolId);
+router.route("/students/:schoolId/:studentClass").get(getAllStudentsClassWise);
 router.route("/student/new").post(createStudent);
 router.route("/student/:id").put(updateStudent).delete(deleteStudent).get(getStudentDetails);
-
-
-
-
-
 module.exports = router
